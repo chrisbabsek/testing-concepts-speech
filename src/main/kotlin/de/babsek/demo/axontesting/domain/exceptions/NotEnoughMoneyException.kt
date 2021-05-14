9f857e1847320ab717442712b791b61d96af8ae8
Christian Babsek <christian.babsek@syna.de>
2021-05-14 12:07:45 +0200

    add endpoints to open bank accounts and to pay in money

--- src/main/kotlin/de/babsek/demo/axontesting/domain/exceptions/NotEnoughMoneyException.kt
+++ src/main/kotlin/de/babsek/demo/axontesting/domain/exceptions/NotEnoughMoneyException.kt
@@ -4,5 +4,5 @@ class NotEnoughMoneyException(
     bankAccountId: String,
     requestedAmount: Double
 ) : RuntimeException(
-    message = "The bank account $bankAccountId balance is too low to transfer an amount of $requestedAmount!"
+    "The bank account $bankAccountId balance is too low to transfer an amount of $requestedAmount!"
 )
